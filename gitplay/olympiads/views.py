f25295e3bbf5902523fd4698c0051151a6d4f690
Alisa-Mikulina <alice.mikulina@gmail.com>
2023-03-19 23:47:31 +0300

    feat: olympiads shown by number on their pages

--- gitplay/olympiads/views.py
+++ gitplay/olympiads/views.py
@@ -13,9 +13,17 @@ def list_olympiads(request):
 
 def show_olympiad(request, id: int):
 
-    context = {
-        'id': id
-    }
+    if id > len(constants.OLYMPIADS) or id < 1:
+        context = {
+            'id': id,
+            'olympiads': constants.OLYMPIADS
+        }
+    else:
+        context = {
+            'id': id,
+            'olympiad': constants.OLYMPIADS[id - 1],
+            'olympiads': constants.OLYMPIADS
+        }
 
     return render(request, 'olympiad.html', context=context)
 
