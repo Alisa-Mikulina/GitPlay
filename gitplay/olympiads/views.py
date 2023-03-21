f5877c23e5d277d042e8bca89e565581c28748f8
Alisa-Mikulina <alice.mikulina@gmail.com>
2023-03-21 20:20:59 +0300

    fix: fixed olympiads view

--- gitplay/olympiads/views.py
+++ gitplay/olympiads/views.py
@@ -18,10 +18,9 @@ def show_olympiad(request, id: int):
     all_olympiads = Olympiad.objects.all()
 
     if id > len(all_olympiads) or id < 1:
-        context = {
-            'id': id,
-            'olympiads': all_olympiads
-        }
+        
+        return render(request, 'olympiad.html')
+
     else:
 
         olympiad = Olympiad.objects.filter(id=id)
@@ -31,7 +30,6 @@ def show_olympiad(request, id: int):
             'id': id,
             'name': olympiad_vals[0][1],
             'lvl': olympiad_vals[0][2],
-            'olympiads': Olympiad.objects.all()
         }
 
     return render(request, 'olympiad.html', context=context)
